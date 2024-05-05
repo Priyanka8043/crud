440c71b85a54409438a36076751b808b5870baf2
Priyanka <priyankaspatil610@gmail.com>
2024-05-05 15:08:38 +0530

    first commit

--- app.js
+++ app.js
@@ -1,153 +1,89 @@
-var form = document.getElementById("myForm"),
-    imgInput = document.querySelector(".img"),
-    file = document.getElementById("imgInput"),
-    userName = document.getElementById("name"),
-    age = document.getElementById("age"),
-    city = document.getElementById("city"),
-    email = document.getElementById("email"),
-    phone = document.getElementById("phone"),
-    post = document.getElementById("post"),
-    sDate = document.getElementById("sDate"),
-    submitBtn = document.querySelector(".submit"),
-    userInfo = document.getElementById("data"),
-    modal = document.getElementById("userForm"),
-    modalTitle = document.querySelector("#userForm .modal-title"),
-    newUserBtn = document.querySelector(".newUser")
+document.addEventListener("DOMContentLoaded", function() {
+    const newUserBtn = document.getElementById("newUserBtn");
+    const modal = document.getElementById("userForm");
+    const submitBtn = document.getElementById("submitBtn");
+    const dataTable = document.getElementById("dataTable").getElementsByTagName('tbody')[0];
+    const form = document.getElementById("myForm");
+    let userData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : [];
+    let isEdit = false;
+    let editIndex = -1;
+    
+    newUserBtn.addEventListener('click', function() {
+        isEdit = false;
+        editIndex = -1;
+        form.reset();
+        modal.style.display = "block";
+    });
 
+    document.querySelector(".close").addEventListener('click', function() {
+        modal.style.display = "none";
+    });
 
-let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : []
-
-let isEdit = false, editId
-showInfo()
-
-newUserBtn.addEventListener('click', ()=> {
-    submitBtn.innerText = 'Submit',
-    modalTitle.innerText = "Fill the Form"
-    isEdit = false
-    imgInput.src = "./image/Profile Icon.webp"
-    form.reset()
-})
-
-
-file.onchange = function(){
-    if(file.files[0].size < 1000000){  // 1MB = 1000000
-        var fileReader = new FileReader();
-
-        fileReader.onload = function(e){
-            imgUrl = e.target.result
-            imgInput.src = imgUrl
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
+    };
 
-        fileReader.readAsDataURL(file.files[0])
-    }
-    else{
-        alert("This file is too large!")
-    }
-}
-
-
-function showInfo(){
-    document.querySelectorAll('.employeeDetails').forEach(info => info.remove())
-    getData.forEach((element, index) => {
-        let createElement = `<tr class="employeeDetails">
-            <td>${index+1}</td>
-            <td><img src="${element.picture}" alt="" width="50" height="50"></td>
-            <td>${element.employeeName}</td>
-            <td>${element.employeeAge}</td>
-            <td>${element.employeeCity}</td>
-            <td>${element.employeeEmail}</td>
-            <td>${element.employeePhone}</td>
-            <td>${element.employeePost}</td>
-            <td>${element.startDate}</td>
-
-
-            <td>
-                <button class="btn btn-success" onclick="readInfo('${element.picture}', '${element.employeeName}', '${element.employeeAge}', '${element.employeeCity}', '${element.employeeEmail}', '${element.employeePhone}', '${element.employeePost}', '${element.startDate}')" data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
-
-                <button class="btn btn-primary" onclick="editInfo(${index}, '${element.picture}', '${element.employeeName}', '${element.employeeAge}', '${element.employeeCity}', '${element.employeeEmail}', '${element.employeePhone}', '${element.employeePost}', '${element.startDate}')" data-bs-toggle="modal" data-bs-target="#userForm"><i class="bi bi-pencil-square"></i></button>
-
-                <button class="btn btn-danger" onclick="deleteInfo(${index})"><i class="bi bi-trash"></i></button>
-                            
-            </td>
-        </tr>`
-
-        userInfo.innerHTML += createElement
-    })
-}
-showInfo()
-
-
-function readInfo(pic, name, age, city, email, phone, post, sDate){
-    document.querySelector('.showImg').src = pic,
-    document.querySelector('#showName').value = name,
-    document.querySelector("#showAge").value = age,
-    document.querySelector("#showCity").value = city,
-    document.querySelector("#showEmail").value = email,
-    document.querySelector("#showPhone").value = phone,
-    document.querySelector("#showPost").value = post,
-    document.querySelector("#showsDate").value = sDate
-}
-
-
-function editInfo(index, pic, name, Age, City, Email, Phone, Post, Sdate){
-    isEdit = true
-    editId = index
-    imgInput.src = pic
-    userName.value = name
-    age.value = Age
-    city.value =City
-    email.value = Email,
-    phone.value = Phone,
-    post.value = Post,
-    sDate.value = Sdate
-
-    submitBtn.innerText = "Update"
-    modalTitle.innerText = "Update The Form"
-}
-
-
-function deleteInfo(index){
-    if(confirm("Are you sure want to delete?")){
-        getData.splice(index, 1)
-        localStorage.setItem("userProfile", JSON.stringify(getData))
-        showInfo()
-    }
-}
-
-
-form.addEventListener('submit', (e)=> {
-    e.preventDefault()
-
-    const information = {
-        picture: imgInput.src == undefined ? "./image/Profile Icon.webp" : imgInput.src,
-        employeeName: userName.value,
-        employeeAge: age.value,
-        employeeCity: city.value,
-        employeeEmail: email.value,
-        employeePhone: phone.value,
-        employeePost: post.value,
-        startDate: sDate.value
+    function showData() {
+        dataTable.innerHTML = "";
+        userData.forEach((user, index) => {
+            const row = dataTable.insertRow();
+            row.innerHTML = `
+                <td>${index + 1}</td>
+                <td>${user.name}</td>
+                <td>${user.city}</td>
+                <td>${user.email}</td>
+                <td>${user.phone}</td>
+                <td>
+                    <button class="btn edit-btn" onclick="editUser(${index})">Edit</button>
+                    <button class="btn delete-btn" onclick="deleteUser(${index})">Delete</button>
+                </td>
+            `;
+        });
     }
 
-    if(!isEdit){
-        getData.push(information)
+    showData();
+
+    submitBtn.addEventListener('click', function() {
+        const name = document.getElementById("name").value;
+        const city = document.getElementById("city").value;
+        const email = document.getElementById("email").value;
+        const phone = document.getElementById("phone").value;
+
+        if (name && city && email && phone) {
+            if (isEdit && editIndex !== -1) {
+                userData[editIndex] = { name, city, email, phone };
+                isEdit = false;
+                editIndex = -1;
+            } else {
+                userData.push({ name, city, email, phone });
+            }
+            localStorage.setItem('userProfile', JSON.stringify(userData));
+            showData();
+            form.reset();
+            modal.style.display = "none";
+        } else {
+            alert("Please fill in all fields!");
+        }
+    });
+
+    window.editUser = function(index) {
+        isEdit = true;
+        editIndex = index;
+        const user = userData[index];
+        document.getElementById("name").value = user.name;
+        document.getElementById("city").value = user.city;
+        document.getElementById("email").value = user.email;
+        document.getElementById("phone").value = user.phone;
+        modal.style.display = "block";
     }
-    else{
-        isEdit = false
-        getData[editId] = information
+
+    window.deleteUser = function(index) {
+        if (confirm("Are you sure you want to delete this user?")) {
+            userData.splice(index, 1);
+            localStorage.setItem('userProfile', JSON.stringify(userData));
+            showData();
+        }
     }
-
-    localStorage.setItem('userProfile', JSON.stringify(getData))
-
-    submitBtn.innerText = "Submit"
-    modalTitle.innerHTML = "Fill The Form"
-
-    showInfo()
-
-    form.reset()
-
-    imgInput.src = "./image/Profile Icon.webp"  
-
-    // modal.style.display = "none"
-    // document.querySelector(".modal-backdrop").remove()
-})
+});
